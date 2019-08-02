c570e21f458a6e731ea483227b799dff9451a1e1
Geovanny Buitrago <geovanny91_1@hotmail.com>
2019-08-01 23:41:51 -0500

    Created some Custom Scopes AuthScope and MainScope, basically 2 annotations, when a component pointed with a scope died also his subcomponents.

--- app/src/main/java/com/gyros/dagger2login/di/ActivityBuildersModule.java
+++ app/src/main/java/com/gyros/dagger2login/di/ActivityBuildersModule.java
@@ -1,9 +1,11 @@
 package com.gyros.dagger2login.di;
 
 import com.gyros.dagger2login.di.auth.AuthModule;
+import com.gyros.dagger2login.di.auth.AuthScope;
 import com.gyros.dagger2login.di.auth.AuthViewModelsModule;
 import com.gyros.dagger2login.di.main.MainFragmentBuildersModule;
 import com.gyros.dagger2login.di.main.MainModule;
+import com.gyros.dagger2login.di.main.MainScope;
 import com.gyros.dagger2login.di.main.MainViewModelsModule;
 import com.gyros.dagger2login.ui.auth.AuthActivity;
 import com.gyros.dagger2login.ui.main.MainActivity;
@@ -14,6 +16,7 @@ import dagger.android.ContributesAndroidInjector;
 @Module
 public abstract class ActivityBuildersModule {
 
+    @AuthScope
     @ContributesAndroidInjector(
             modules = {
                     AuthViewModelsModule.class,
@@ -22,6 +25,7 @@ public abstract class ActivityBuildersModule {
     )
     abstract AuthActivity contributeAuthActivity();
 
+    @MainScope
     @ContributesAndroidInjector(
             modules = {
                     MainFragmentBuildersModule.class,
---
+++ app/src/main/java/com/gyros/dagger2login/di/auth/AuthScope.java
@@ -0,0 +1,13 @@
+package com.gyros.dagger2login.di.auth;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+import javax.inject.Scope;
+
+@Scope
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+public @interface AuthScope {
+}
